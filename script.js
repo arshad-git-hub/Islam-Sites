810da4c4b78be95c499c4e691472d237d9e91df8
Arshad Waseem <arshad-git-hub@example.com>
2025-08-31 00:08:36 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let ham_btn = document.getElementById("hamburger");
+let nav_list = document.getElementById("nav-list")
+
+ham_btn.addEventListener( 'click', () => {
+    nav_list.classList.toggle('active');
+})
+
+let sl1 = document.getElementById('sl1')
+
+
